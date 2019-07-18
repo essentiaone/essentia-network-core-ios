9e61c49254c91b2f6552e5bd10ae5ec46a575017
impul <pavlo.bojkoo@gmail.com>
2019-07-18 12:24:54 +0300

    Implement RxNetworkManager

--- essentia-network-core-ios/Core/RxManager/RxNetworkManager.swift
+++ essentia-network-core-ios/Core/RxManager/RxNetworkManager.swift
@@ -10,24 +10,39 @@ import Foundation
 import RxSwift
 
 public class RxNetworkManager: RxNetworkManagerInterface {
-    private var urlSession: URLSession
-    private var serverUrl: String
+    private let networkManager: NetworkManager
     
     init(url: String) {
-        urlSession = URLSession.defaultNetworkSession
-        self.serverUrl = url
+        self.networkManager = NetworkManager(url)
     }
     
     public func request<SuccessModel: Decodable>(_ request: RequestProtocol) -> Observable<SuccessModel> {
         return Observable.create({ (observer) -> Disposable in
-            
+            self.networkManager.request(request, result: { (result: NetworkResult<SuccessModel>) in
+                switch result {
+                case .success(let object):
+                    observer.onNext(object)
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            })
             return Disposables.create()
         })
     }
     
     public func requestData(_ request: RequestProtocol) -> Observable<Data> {
         return Observable.create({ (observer) -> Disposable in
-            
+            self.networkManager.requestData(request, result: { (data, error) in
+                guard let data = data else {
+                    guard let error = error else {
+                        observer.onError(EssentiaNetworkError.unknownError)
+                        return
+                    }
+                    observer.onError(error)
+                    return
+                }
+                observer.onNext(data)
+            })
             return Disposables.create()
         })
     }
